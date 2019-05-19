c095365e32026cadc5734c9774ddd195710a1058
Mariam Joan <mariamjoan@Mariams-MBP-2.lan1>
2019-05-18 21:22:25 -0700

    problem 1 and 2

--- problem_2.py
+++ problem_2.py
@@ -1,30 +1,4 @@
 import os
-# os.path.isdir(path)
-# os.path.isfile(path)
-# os.listdir(directory)
-# os.path.join(...)
-# Listing Out only directories
-# directories = filter(os.path.isdir, os.listdir(os.curdir))
-
-# Listing Out only files in current directory
-# files = filter(os.path.isfile, os.listdir(os.curdir))
-# list_of_paths = os.listdir("./testdir")
-# print('listofpaths', list_of_paths)
-# for files in list_of_paths:
-#     file_confirmed = os.path.isfile(files)
-#     print('file', file_confirmed, suffix)
-#     print('files', files)
-
-#     if file_confirmed:
-#         print(files.endswith(suffix))
-# directories = os.path.join(os.path.isdir, os.listdir(os.curdir))
-# directories = [ x for x in os.listdir('.') if os.path.isdir(x) ]
-# return [x[0] for x in os.walk(list_of_paths)]
-# os.path.isdir(os.path.join(a_dir, name))
-# dir_list = next(os.walk('.'))[2]
-
-# print('dirlist', [x[0] for x in os.walk(file_confirmed)])
-
 
 def find_files(suffix, path):
     """
@@ -42,14 +16,3 @@ def find_files(suffix, path):
     return None
 
 find_files('.c', '/Users/mariamjoan/Notebooks/testdir')
-
-
-# def find_files(suffix, path): 
-#    files_found = [] 
-#    recursive_file_search(suffix, path, files_found) 
-#    return files_found
-
-# import os
-# def get_immediate_subdirectories(a_dir):
-#     return [name for name in os.listdir(a_dir)
-#             if os.path.isdir(os.path.join(a_dir, name))]
